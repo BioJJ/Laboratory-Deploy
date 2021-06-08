fbd3958bd3783e91b71f12f073cbc5b311677914
Jefferson Coelho 🐱‍👤 <bio.jeffcoelho@gmail.com>
2021-06-08 19:16:41 -0400

    ✨ finishing laboratory crud

--- src/laboratory/laboratory.service.ts
+++ src/laboratory/laboratory.service.ts
@@ -12,10 +12,45 @@ export class LaboratoryService {
   ) {}
 
   async findAll(): Promise<LaboratoryEntity[]> {
-    return await this.laboratoryRepository.find();
+    return await this.laboratoryRepository.find({
+      where: { status: 'ACTIVATE' },
+    });
   }
 
+  async find(laboratoryId: number): Promise<Laboratory> {
+    const { id, nome, endereco, status } =
+      await this.laboratoryRepository.findOne(laboratoryId);
+
+    const response: Laboratory = {
+      id,
+      nome,
+      endereco,
+      status,
+    };
+    return response;
+  }
   async create(laboratory: Laboratory): Promise<LaboratoryEntity> {
     return await this.laboratoryRepository.save(laboratory);
+  }
+
+  async update(laboratory: Laboratory): Promise<LaboratoryEntity> {
+    const lab = await this.find(laboratory.id);
+
+    lab.nome = laboratory.nome ? laboratory.nome : lab.nome;
+    lab.endereco = laboratory.endereco ? laboratory.endereco : lab.endereco;
+    lab.status = laboratory.status ? laboratory.status : lab.status;
+    return await this.laboratoryRepository.save(lab);
+  }
+
+  async delete(id: number): Promise<void> {
+    await this.laboratoryRepository.delete({ id });
+  }
+
+  async activate(id: number): Promise<void> {
+    await this.laboratoryRepository.update(id, { status: 'ACTIVATE' });
+  }
+
+  async inactivate(id: number): Promise<void> {
+    await this.laboratoryRepository.update(id, { status: 'INACTIVATE' });
   }
 }
